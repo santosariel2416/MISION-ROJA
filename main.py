abd4ef5d652b8242bceb41b27f873cc47b12d3a4
Jesus Ariel <santosariel2416@gmail.com>
2026-02-17 13:12:26 -0400

    cree la ventana del juego con su ancho y altura, tambien el tiempo para los FPS, variable de control y un bucle y tambien le puse un fondo rojo a la pantalla

--- main.py
+++ main.py
@@ -1,6 +1,43 @@
-#Jesus Ariel Santos 24-EISN-2-034
-
-#Importe la libreria pygame y sys que es una libreria de python y sirve para cerrar el programa correctamente. 
+# Jesus Ariel Santos 24-EISN-2-034
 
 import pygame
-import sys 
+import sys
+
+def iniciar_juego():
+
+    # Iniciar pygame
+    pygame.init()
+
+    # Tamaño de ventana
+    Ancho = 1000
+    Alto = 700
+
+    pantalla = pygame.display.set_mode((Ancho, Alto))
+    pygame.display.set_caption("Mision Roja")
+
+    # tiempo para los FPS
+    tiempo = pygame.time.Clock()
+
+    # Variable de control
+    corriendo = True
+
+    # Bucle principal
+    while corriendo:
+
+        tiempo.tick(60)
+
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                corriendo = False
+
+        # Fondo rojo oscuro
+        pantalla.fill((60, 0, 0))
+
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    iniciar_juego()
